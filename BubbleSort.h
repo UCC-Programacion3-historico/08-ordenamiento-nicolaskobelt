785eaa7bb0c0885eb8b896159877fa6a67daa088
nicolaskobelt <kobeltnicolas@me.com>
2017-10-25 10:45:38 -0300

    Bubble Listo

--- BubbleSort.h
+++ BubbleSort.h
@@ -3,7 +3,26 @@
 
 
 template <class T>
-void bubbleSort(T *vect, unsigned long tamanio);
+void bubbleSort(T *vect, unsigned long tamanio){
+    T aux;
+    int flag = 0;
+
+    while (flag == 0) {
+        for (unsigned long int i = 0; i < tamanio - 1; i++) {
+
+            for (unsigned long int j = 1; j < tamanio; j++) {
+
+                if (vect[i] < vect[j]) {
+                    aux = vect[i];
+                    vect[i] = vect[j];
+                    vect[j] = aux;
+                    flag = 0;
+                }else
+                    flag = 1;
+            }
+        }
+    }
+}
 
 
 #endif //BUBBLESORT_H
